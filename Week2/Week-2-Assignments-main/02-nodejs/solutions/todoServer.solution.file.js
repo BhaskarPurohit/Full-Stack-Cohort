55816f05b87eb5ba693dff447a77a45562584c08
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-10-09 14:14:24 +0530

    path

--- Week2/Week-2-Assignments-main/02-nodejs/solutions/todoServer.solution.file.js
+++ Week2/Week-2-Assignments-main/02-nodejs/solutions/todoServer.solution.file.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
+const path = require("path")
 
 const app = express();
 
@@ -74,6 +75,11 @@ app.delete('/todos/:id', (req, res) => {
 app.use((req, res, next) => {
   res.status(404).send();
 });
+
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname,"index.html"))
+})
+
 console.log("app is listening")
 // module.exports = app;
 app.listen(3000)
