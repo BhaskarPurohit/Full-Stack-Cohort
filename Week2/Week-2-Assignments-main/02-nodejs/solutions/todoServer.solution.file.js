f0ee96620218b2e1e88d96114f476b13096259aa
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-10-09 13:41:16 +0530

    new commits

--- Week2/Week-2-Assignments-main/02-nodejs/solutions/todoServer.solution.file.js
+++ Week2/Week-2-Assignments-main/02-nodejs/solutions/todoServer.solution.file.js
@@ -74,5 +74,6 @@ app.delete('/todos/:id', (req, res) => {
 app.use((req, res, next) => {
   res.status(404).send();
 });
-
-module.exports = app;
+console.log("app is listening")
+// module.exports = app;
+app.listen(3000)
